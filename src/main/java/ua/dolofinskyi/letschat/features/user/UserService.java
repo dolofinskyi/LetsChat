045ac44a3705efb94ffa082606c6280ff4ade0a6
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2024-01-05 15:11:06 +0200

    Rename CrudService package

--- src/main/java/ua/dolofinskyi/letschat/features/user/UserService.java
+++ src/main/java/ua/dolofinskyi/letschat/features/user/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ua.dolofinskyi.letschat.features.service.CrudService;
+import ua.dolofinskyi.letschat.features.crud.CrudService;
 
 import java.util.*;
 
