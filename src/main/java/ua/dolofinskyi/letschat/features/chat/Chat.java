b772a0cc15555340970f43925a8bd0153b35eb8c
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2024-01-10 13:55:44 +0200

    Add PrivateChat and GroupChat

--- src/main/java/ua/dolofinskyi/letschat/features/chat/Chat.java
+++ src/main/java/ua/dolofinskyi/letschat/features/chat/Chat.java
@@ -1,18 +1,8 @@
 package ua.dolofinskyi.letschat.features.chat;
 
-import lombok.Builder;
-import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
 
-import java.util.Set;
-
-@Data
-@RedisHash
-@Builder
-public class Chat {
-    @Id
-    private final String id;
-    private final Set<String> users;
-    private final Set<String> messages;
+public interface Chat {
+    boolean isPrivate();
 }
+
+
