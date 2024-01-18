05ac7f125055e02c4f564e2a119bf4a2664bfbba
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2024-01-18 22:28:31 +0200

    fix Mapper feature

--- src/main/java/ua/dolofinskyi/letschat/features/mapper/Mapper.java
+++ src/main/java/ua/dolofinskyi/letschat/features/mapper/Mapper.java
@@ -1,6 +1,16 @@
 package ua.dolofinskyi.letschat.features.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public interface Mapper<E, D> {
     E toEntity(D dto);
     D toDto(E entity);
+    default List<E> toEntities(List<D> dtos) {
+        return dtos.stream().map(this::toEntity).toList();
+    }
+    default List<D> toDtos(List<E> entities) {
+        return entities.stream().map(this::toDto).toList();
+    }
 }
