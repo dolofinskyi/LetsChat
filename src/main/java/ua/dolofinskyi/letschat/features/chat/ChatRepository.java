4f638471bb4aa220908e7771ab8f307552d0ca2d
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2024-01-23 22:55:30 +0200

    switch CrudRepository to JpaRepository

--- src/main/java/ua/dolofinskyi/letschat/features/chat/ChatRepository.java
+++ src/main/java/ua/dolofinskyi/letschat/features/chat/ChatRepository.java
@@ -1,9 +1,9 @@
 package ua.dolofinskyi.letschat.features.chat;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRepository extends CrudRepository<Chat, String> {
+public interface ChatRepository extends JpaRepository<Chat, String> {
 
 }
--- src/main/java/ua/dolofinskyi/letschat/features/user/UserRepository.java
+++ src/main/java/ua/dolofinskyi/letschat/features/user/UserRepository.java
@@ -1,9 +1,9 @@
 package ua.dolofinskyi.letschat.features.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
 
 }
