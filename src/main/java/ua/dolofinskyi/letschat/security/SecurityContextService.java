819df05388a766970a02c47642b738077ca116dc
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2024-01-25 18:24:50 +0200

    fix user search

--- src/main/java/ua/dolofinskyi/letschat/security/SecurityContextService.java
+++ src/main/java/ua/dolofinskyi/letschat/security/SecurityContextService.java
@@ -1,6 +1,5 @@
 package ua.dolofinskyi.letschat.security;
 
-import com.sun.security.auth.UserPrincipal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,10 +7,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityContextService {
-
-    public UserPrincipal getUserPrincipal() {
-        return (UserPrincipal) getAuthentication().getPrincipal();
-    }
 
     public Authentication getAuthentication() {
         return getContext().getAuthentication();
