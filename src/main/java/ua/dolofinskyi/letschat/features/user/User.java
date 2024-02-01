3ec5ce2f221007db8325f4daee80d6116b593d07
Denys Olofinskyi <denis.olofinskyi@gmail.com>
2024-02-01 18:48:32 +0200

    fix User getAuthorities()

--- src/main/java/ua/dolofinskyi/letschat/features/user/User.java
+++ src/main/java/ua/dolofinskyi/letschat/features/user/User.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -31,7 +30,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.emptyList();
+        return new ArrayList<>();
     }
 
     @Override
